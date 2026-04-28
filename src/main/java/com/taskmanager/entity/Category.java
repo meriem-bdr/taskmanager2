eae3a2e3d6c56707a56b5837877c2026de8821f2
rahma149 <bacrahma67@gmail.com>
2026-04-28 15:35:57 +0200

    Create Category.java

---
+++ src/main/java/com/taskmanager/entity/Category.java
@@ -0,0 +1,42 @@
+package com.taskmanager.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Item> items = new java.util.ArrayList<>();
+    // ===== GETTERS & SETTERS =====
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
