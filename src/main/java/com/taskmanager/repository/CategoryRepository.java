edef35ac5950496ac767aefed50c695ec4125c85
rahma149 <bacrahma67@gmail.com>
2026-04-28 15:38:04 +0200

    Create CategoryRepository.java

---
+++ src/main/java/com/taskmanager/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.taskmanager.repository;
+
+import com.taskmanager.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
